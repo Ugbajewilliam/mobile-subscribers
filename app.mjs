d4782143bfde4151a7ee97f52237753574caf8af
Ugo Chiori <ugochiori@gmail.com>
2022-08-18 15:12:04 +0100

    uncommented line of code

--- app.mjs
+++ app.mjs
@@ -1,34 +1,26 @@
 function startApp() {
-  // Your entire app should not necessarily be coded inside this 
-  // single function (though there's no penalty for that), 
+  // Your entire app should not necessarily be coded inside this
+  // single function (though there's no penalty for that),
   // so create and use/call additional functions from here
 
   // pls remove the below and make some magic in here!
   // console.log('make magic in here!');
 
-
-  const carrierCode = document.getElementById('carrier_code');
-  const logo = document.getElementById('logo');
-  const resetButton = document.getElementById('reset');
+  const carrierCode = document.getElementById("carrier_code");
+  const logo = document.getElementById("logo");
+  const resetButton = document.getElementById("reset");
 
   function reset() {
-    carrierCode.value = ''
+    carrierCode.value = "";
   }
-  resetButton.addEventListener('click', reset)
+  resetButton.addEventListener("click", reset);
 
-  // carrierCode.onchange = function (e) {
-  //   console.log(e.target.value)
-  // }
-
-
-  carrierCode.addEventListener('change', function(event) {
-    if (event.target.value.length == 11 ) {
-   
-    console.log(event.target.value)
-    
-      let theValue = event.target.value.toString()
-      let newValue = theValue.slice(0, 4)
-      if (newValue == "0803" ||
+  carrierCode.addEventListener("change", function (event) {
+    if (event.target.value.length == 11) {
+      let theValue = event.target.value.toString();
+      let newValue = theValue.slice(0, 4);
+      if (
+        newValue == "0803" ||
         newValue == "0806" ||
         newValue == "0813" ||
         newValue == "0706" ||
@@ -39,14 +31,13 @@ function startApp() {
         newValue == "0810" ||
         newValue == "0704" ||
         newValue == "0913"
-
       ) {
-        logo.setAttribute("src", "MTNLOGO.png")
-        return
-
+        logo.setAttribute("src", "MTNLOGO.png");
+        return;
       }
 
-      if (newValue == "0802" ||
+      if (
+        newValue == "0802" ||
         newValue == "0812" ||
         newValue == "0808" ||
         newValue == "0701" ||
@@ -56,11 +47,11 @@ function startApp() {
         newValue == "0907" ||
         newValue == "0902"
       ) {
-        logo.setAttribute("src", "Airtel.png")
-        return
-
+        logo.setAttribute("src", "Airtel.png");
+        return;
       }
-      if (newValue == "0805" ||
+      if (
+        newValue == "0805" ||
         newValue == "0807" ||
         newValue == "0811" ||
         newValue == "0905" ||
@@ -68,25 +59,23 @@ function startApp() {
         newValue == "0915" ||
         newValue == "0705"
       ) {
-        logo.setAttribute("src", "Glo.webp")
-        return
-
+        logo.setAttribute("src", "Glo.webp");
+        return;
       }
-      if (newValue == "0809" ||
+      if (
+        newValue == "0809" ||
         newValue == "0909" ||
         newValue == "0817" ||
         newValue == "0818" ||
         newValue == "0908"
       ) {
-        logo.setAttribute("src", "9mobile.jpeg")
-        return
-
+        logo.setAttribute("src", "9mobile.jpeg");
+        return;
       }
-
     }
-  })
+  });
 }
 
 // ======= DO NOT EDIT ============== //
 export default startApp;
-  // ======= EEND DO NOT EDIT ========= //
+// ======= EEND DO NOT EDIT ========= //
